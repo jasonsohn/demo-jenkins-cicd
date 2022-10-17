ae5d4bbae4e42b13711ed0a9ef67c12c697d84dd
손재승 (헬스/환경플랫폼개발TF) <jason.sohn@kt.com>
2022-10-17 21:13:36 +0900

    first commit

---
+++ src/test/java/com/example/demojenkinscicd/DemoJenkinsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
